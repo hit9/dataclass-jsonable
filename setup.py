7c3bae7ffa6c4a789b83dda0573f8aa8f536041e
hit9 <hit9@icloud.com>
2022-08-09 00:56:04 +0800

    Add a simple setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="dataclass-jsonable",
+    version="0.0.1",
+    author="hit9",
+    author_email="hit9@icloud.com",
+    description="Conversions between dataclasses and jsonable dictionaries.",
+    license="BSD",
+    keywords="dataclasses,json,jsonable",
+    url="https://github.com/hit9/dataclass-jsonable",
+    py_modules=["dataclass_jsonable"],
+    python_requires=">=3.7",
+)
