47de7f0cdd8ece5efecabd03967df29bf64bf552
hit9 <hit9@icloud.com>
2022-08-09 10:56:35 +0800

    Use package instead of module

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup  # type: ignore
 
 setup(
     name="dataclass-jsonable",
-    version="0.0.1",
+    version="0.0.2",
     author="hit9",
     author_email="hit9@icloud.com",
     description="Conversions between dataclasses and jsonable dictionaries.",
     license="BSD",
     keywords="dataclasses,json,jsonable",
     url="https://github.com/hit9/dataclass-jsonable",
-    py_modules=["dataclass_jsonable"],
+    packages=["dataclass_jsonable"],
+    include_package_data=True,
+    zip_safe=False,
     python_requires=">=3.7",
 )
