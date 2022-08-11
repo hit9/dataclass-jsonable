ac0d197c7dedb65dea9b379c5ce8a87f0e5bf5a1
hit9 <hit9@icloud.com>
2022-08-11 16:38:02 +0800

    Add test_forwardref

---
+++ tests/test_forwardref.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from dataclass_jsonable import J, json_options
+
+
+@dataclass
+class Tree(J):
+    __default_json_options__ = json_options(omitempty=True)
+    name: str
+    left: Optional["Tree"] = None
+    right: Optional["Tree"] = None
+
+
+@dataclass
+class Node(J):
+    data: int
+    children: List["Node"] = field(default_factory=list)
+
+
+def test_forward_ref_tree():
+    tree = Tree("root", left=Tree("left"), right=Tree("right"))
+    d = {"name": "root", "left": {"name": "left"}, "right": {"name": "right"}}
+    assert tree.json() == d
+    Tree.from_json(d) == tree
+
+
+def test_forward_ref_node():
+    node = Node(1, children=[Node(2), Node(3), Node(4, children=[Node(5)])])
+    d = {
+        "data": 1,
+        "children": [
+            {"data": 2, "children": []},
+            {"data": 3, "children": []},
+            {"data": 4, "children": [{"data": 5, "children": []}]},
+        ],
+    }
+    assert node.json() == d
