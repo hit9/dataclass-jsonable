65e975703759109f14732c8fd369175bb2148cba
hit9 <hit9@icloud.com>
2022-08-11 22:24:16 +0800

    Add test_forward_ref_generics

--- tests/test_forwardref.py
+++ tests/test_forwardref.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 from dataclass_jsonable import J, json_options
 
@@ -12,17 +12,17 @@ class Tree(J):
     right: Optional["Tree"] = None
 
 
-@dataclass
-class Node(J):
-    data: int
-    children: List["Node"] = field(default_factory=list)
-
-
 def test_forward_ref_tree():
     tree = Tree("root", left=Tree("left"), right=Tree("right"))
     d = {"name": "root", "left": {"name": "left"}, "right": {"name": "right"}}
     assert tree.json() == d
     Tree.from_json(d) == tree
+
+
+@dataclass
+class Node(J):
+    data: int
+    children: List["Node"] = field(default_factory=list)
 
 
 def test_forward_ref_node():
@@ -36,3 +36,16 @@ def test_forward_ref_node():
         ],
     }
     assert node.json() == d
+
+
+@dataclass
+class Simple(J):
+    data: "Dict[str, Any]"
+    n: "Optional[int]"
+
+
+def test_forward_ref_generics():
+    s = Simple(data={"x": 1}, n=2)
+    assert s.json() == {"data": {"x": 1}, "n": 2}
+    s1 = Simple.from_json({"data": {"x": 1}, "n": 2})
+    assert s1 == s
