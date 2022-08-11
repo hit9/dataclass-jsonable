e554d733cd8893be4fda39dcbb8a70623104fbf1
hit9 <hit9@icloud.com>
2022-08-11 22:43:01 +0800

    Add enum to tests

--- tests/test_basic_types.py
+++ tests/test_basic_types.py
@@ -1,8 +1,19 @@
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from decimal import Decimal
+from enum import Enum, IntEnum
 
 from dataclass_jsonable import J
+
+
+class E1(IntEnum):
+    A = 1
+    B = 2
+
+
+class E2(Enum):
+    A = "A"
+    B = "B"
 
 
 @dataclass
@@ -15,6 +26,8 @@ class S1(J):
     f: Decimal
     g: datetime
     h: timedelta
+    e1: E1
+    e2: E2
 
 
 def test_basic_types():
@@ -27,6 +40,8 @@ def test_basic_types():
         f=Decimal("1.2"),
         g=datetime(2022, 1, 1, 1, 1, 1),
         h=timedelta(days=1),
+        e1=E1.A,
+        e2=E2.B,
     )
     x = {
         "a": 1,
@@ -37,6 +52,8 @@ def test_basic_types():
         "f": "1.2",
         "g": 1640970061,
         "h": 86400,
+        "e1": 1,
+        "e2": "B",
     }
     assert s.json() == x
     assert S1.from_json(x) == s
